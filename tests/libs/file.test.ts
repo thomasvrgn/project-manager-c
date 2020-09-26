62143b17a54f0d151063298a0d77e632a1d84011
NessMC <contact@nessmc.fr>
2020-09-26 11:51:30 +0200

    🧼 Tests cleaned up.

--- tests/libs/file.test.ts
+++ tests/libs/file.test.ts
@@ -1,13 +1,15 @@
 import Reader from 'libs/file';
 
-describe('Reader library testing', () => {
-  test('should return text content', async () => {
-    const reader: Reader = new Reader('tests/libs/content/test.txt');
-    const content: string = await reader.read();
-    expect(content).toEqual('bruh');
-  });
-  test('should throw an error when file does not exists', async () => {
-    const reader: Reader = new Reader('fileThatDoesNotExist');
-    expect(reader.read()).rejects.toThrowError();
+describe('#Reader library testing', () => {
+  describe('#Reading', () => {
+    test('should return text content', async () => {
+      const reader: Reader = new Reader('tests/libs/content/test.txt');
+      const content: string = await reader.read();
+      expect(content).toEqual('bruh');
+    });
+    test('should throw an error when file does not exists', async () => {
+      const reader: Reader = new Reader('fileThatDoesNotExist');
+      expect(reader.read()).rejects.toThrowError();
+    });
   });
 });
